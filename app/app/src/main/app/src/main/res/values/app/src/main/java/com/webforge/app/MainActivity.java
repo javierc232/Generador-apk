be62a35fe440da3b8c57dd4e2b22186b90827424
javierc232 <76014414+javierc232@users.noreply.github.com>
2026-05-05 03:43:27 -0300

    Update MainActivity.java

--- app/app/src/main/app/src/main/res/values/app/src/main/java/com/webforge/app/MainActivity.java
+++ app/app/src/main/app/src/main/res/values/app/src/main/java/com/webforge/app/MainActivity.java
@@ -3,7 +3,7 @@ package com.webforge.app;
 import android.content.Intent;
 import android.net.VpnService;
 import android.os.Bundle;
-import android.webkit.JavascriptInterface; // Importante para conectar con el HTML
+import android.webkit.JavascriptInterface; 
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -11,17 +11,15 @@ public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // Esta línea registra la "puerta" para que el HTML pueda hablar con Java
+        // ESTA LÍNEA ES LA QUE QUITA EL ERROR DEL BOTÓN
         this.bridge.getWebView().addJavascriptInterface(this, "AndroidVPN");
     }
 
-    // El @JavascriptInterface es el que permite que el HTML vea esta función
     @JavascriptInterface
     public void prepararVPN() {
         Intent intent = VpnService.prepare(this);
         if (intent != null) {
-            startActivityForResult(intent, 1); // Cambiado a 1 para mejor control
+            startActivityForResult(intent, 1);
         } else {
             onActivityResult(1, RESULT_OK, null);
         }
